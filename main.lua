9d74f589f245940e4da52a9094c79660567297bd
Lime <limeroblox2019@gmail.com>
2025-10-02 18:10:48 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,3 +5,6 @@ Planned Features:
 Implements automatic game support and UI notifications based on GameID or PlaceID.
 
 ]]
+
+
+local StarterGui = game:GetService("StarterGui") StarterGui:SetCore("SendNotification", { Title = "Haxel.lua", Text = "Hey, This Doesn't Work Yet! \n\(Still In DEV)", Duration = 5 })
