72357f9c7b91fa64193f2b9b0c8c7c0314799d72
Lime <limeroblox2019@gmail.com>
2025-10-03 19:46:06 -0400

    Update notification_startup.lua

--- core/notification_startup.lua
+++ core/notification_startup.lua
@@ -1,2 +1,6 @@
+local NotificationLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/lobox920/Notification-Library/Main/Library.lua"))()
+
 -- This runs automatically when the script is executed.
 -- It does not use the WindUI notification component.
+
+NotificationLibrary:SendNotification("Info", "Anazling...", 5)
