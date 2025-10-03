c2192b09e299494687b9acf674e2ddecb9ee2553
Lime <limeroblox2019@gmail.com>
2025-10-03 19:58:48 -0400

    Delete core/notification_startup.lua

--- core/notification_startup.lua
+++
@@ -1,6 +0,0 @@
-local NotificationLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/lobox920/Notification-Library/Main/Library.lua"))()
-
--- This runs automatically when the script is executed.
--- It does not use the WindUI notification component.
-
-NotificationLibrary:SendNotification("Info", "Anazling...", 5)
