fdb91f37395c3c006e3d524f0733218dadee3ff9
Lime <limeroblox2019@gmail.com>
2025-10-06 15:02:01 -0400

    Update trashfur_outbreak.lua

--- games/trashfur_outbreak.lua
+++ games/trashfur_outbreak.lua
@@ -9,11 +9,11 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/limeroblox/haxel.lua/
 -- Place Holders to see if this method works
 
 -- Tabs
-local CombatTab     = Window:CreateTab(Localization["COMBAT"])
-local VisualsTab    = Window:CreateTab(Localization["VISUALS"])
-local MiscTab       = Window:CreateTab(Localization["MISC"])
-local AppearanceTab = Window:CreateTab(Localization["APPEARANCE"])
-local ConfigTab     = Window:CreateTab(Localization["CONFIG"])
+local CombatTab     = Window:CreateTab(Localization.Translations[lang]["COMBAT"])
+local VisualsTab    = Window:CreateTab(Localization.Translations[lang]["VISUALS"])
+local MiscTab       = Window:CreateTab(Localization.Translations[lang]["MISC"])
+local AppearanceTab = Window:CreateTab(Localization.Translations[lang]["APPEARANCE"])
+local ConfigTab     = Window:CreateTab(Localization.Translations[lang]["CONFIG"])
 
 
 -- === Managers===
