df9ab5e9f4a9ac0e0eae0f4cb92f93417967c441
Lime <limeroblox2019@gmail.com>
2025-10-06 14:47:06 -0400

    Update trashfur_outbreak.lua

--- games/trashfur_outbreak.lua
+++ games/trashfur_outbreak.lua
@@ -6,3 +6,20 @@ MAIN UI
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/limeroblox/haxel.lua/refs/heads/main/core/ui.lua"))()
 
+-- Place Holders to see if this method works
+
+-- Tabs
+local CombatTab = Window:CreateTab(Localization:Translate("loc:COMBAT"))
+local VisualsTab = Window:CreateTab(Localization:Translate("loc:VISUALS"))
+local MiscTab = Window:CreateTab(Localization:Translate("loc:MISC"))
+local AppearanceTab = Window:CreateTab(Localization:Translate("loc:APPEARANCE"))
+local ConfigTab = Window:CreateTab(Localization:Translate("loc:CONFIG"))
+
+
+-- === Managers===
+local ThemeManager = WindUI:CreateThemeManager(Window)
+local ConfigManager = WindUI:CreateConfigManager(Window)
+ThemeManager:SetFolder("Haxel/Themes")
+ConfigManager:SetFolder("Haxel/Configs")
+
+ConfigManager:Load()
