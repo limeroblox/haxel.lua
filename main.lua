0430c62d361199743778f8ab8475ba6203e0ffc2
Lime <limeroblox2019@gmail.com>
2025-10-20 01:39:26 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -6,7 +6,6 @@ Implements automatic game support and UI notifications based on GameID or PlaceI
 local NotificationLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/lobox920/Notification-Library/Main/Library.lua"))()
 local MarketplaceService = game:GetService("MarketplaceService")
 local PlaceId = game.PlaceId
-local Name = ""
 local Success, GameInfo = pcall(function()
     return MarketplaceService:GetProductInfo(PlaceId)
 end)
@@ -21,15 +20,12 @@ NotificationLibrary:SendNotification("Info", "Game Found: " .. GameName, 3)
 -- Match PlaceId -> Script
 if PlaceId == 123456789 then
     NotificationLibrary:SendNotification("Info", "Script Path:\n\\haxel.lua/refs/heads/main/games/blackout_revival.lua", 5)
-    Name = "Blackout: Revival"
     loadstring(game:HttpGet("https://raw.githubusercontent.com/limeroblox/haxel.lua/refs/heads/main/games/blackout_revival.lua"))()
 elseif PlaceId == 5987922834 then
     NotificationLibrary:SendNotification("Info", "Script Path:\n\\haxel.lua/refs/heads/main/games/trashfur_outbreak.lua", 5)
-    Name = "Transfur Outbreak"
     loadstring(game:HttpGet("https://raw.githubusercontent.com/limeroblox/haxel.lua/refs/heads/main/games/trashfur_outbreak.lua"))()
 elseif PlaceId == 111222333 then
     NotificationLibrary:SendNotification("Info", "Script Path:\n\\haxel.lua/refs/heads/main/games/pressure.lua", 5)
-    Name = "Pressure"
     loadstring(game:HttpGet("https://raw.githubusercontent.com/limeroblox/haxel.lua/refs/heads/main/games/pressure.lua"))()
 else
     NotificationLibrary:SendNotification("Error", "No supported script found for: " .. GameName, 5)
