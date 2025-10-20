dab9e21a5e899f19af374473c8ccab489f275e61
Lime <limeroblox2019@gmail.com>
2025-10-19 23:01:20 -0400

    Update ui.lua

--- core/ui.lua
+++ core/ui.lua
@@ -232,7 +232,7 @@ local Localization = HaxelUI:Localization({
 
 -- Window setup
 
-local title = ""
+local title 
 local Window = HaxelUI:CreateWindow({
     Title                       = title, -- Changes based on the game, (example: Bat.lua | Bed Wars)
     Icon                        = "https://itaku.ee/api/media/gallery_imgs/nikoty_FG55uER/xl.jpg", -- Icon localization not needed, I guess :P, also why is some nsfw, like wth man :(
