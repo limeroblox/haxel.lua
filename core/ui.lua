bd89108d6e961c33abf2e92d79ec76d42a8d3e1b
Lime <limeroblox2019@gmail.com>
2025-12-15 08:30:03 +0000

    Update ui.lua

--- core/ui.lua
+++ core/ui.lua
@@ -5,9 +5,6 @@ It does not include any game-specific elements.
 
 ]]
 
--- Using WindUi Cuz I like it :p
-
--- Also as they said, do not edit the main src from there.
 local repo = "https://raw.githubusercontent.com/deividcomsono/Obsidian/main/" 
 local Library = loadstring(game:HttpGet(repo .. "Library.lua"))() 
 local ThemeManager = loadstring(game:HttpGet(repo .. "addons/ThemeManager.lua"))() 
