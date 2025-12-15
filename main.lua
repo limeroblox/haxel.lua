3afdabd9a32146d4a947c6f1fb36969aeb7eb904
Lime <limeroblox2019@gmail.com>
2025-12-15 13:52:24 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,18 @@
+local BlacklistedGames = { -- Numbers Are Place Holders
+    [999999999] = true, 
+    [888888888] = true, 
+}
+local SupportedGames = {
+    [123456789]    = "blackout_revival.lua", -- Place Holder Number
+    [5987922834]   = "trashfur_outbreak.lua",
+    [111222333]    = "pressure.lua",         -- Place Holder Number
+}
+local NotificationLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/lobox920/Notification-Library/Main/Library.lua"))()
+local BaseURL             = "https://raw.githubusercontent.com/limeroblox/haxel.lua/refs/heads/main/games/"
+local MarketplaceService  = game:GetService("MarketplaceService")
+local PlaceId             = game.PlaceId
+local Success, GameInfo   = pcall(function() return MarketplaceService:GetProductInfo(PlaceId) end)
+local GameName = Success and GameInfo.Name or "Unknown Game"
 local Localization = {
     analyzing      = "Analyzing...",
     gameFound      = "Game Found: %s",
@@ -5,24 +20,6 @@ local Localization = {
     blacklisted    = "Game is blacklisted: %s",
     loadingScript  = "Script Path:\n%s"
 }
-local BlacklistedGames = {
-    [999999999] = true, 
-    [888888888] = true, 
-}
-local SupportedGames = {
-    [123456789]    = "blackout_revival.lua",
-    [5987922834]   = "trashfur_outbreak.lua",
-    [111222333]    = "pressure.lua",
-}
-local NotificationLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/lobox920/Notification-Library/Main/Library.lua"))()
-local BaseURL             = "https://raw.githubusercontent.com/limeroblox/haxel.lua/refs/heads/main/games/"
-local MarketplaceService  = game:GetService("MarketplaceService")
-local PlaceId             = game.PlaceId
-local Success, GameInfo   = pcall(function()
-    return MarketplaceService:GetProductInfo(PlaceId)
-end)
-local GameName = Success and GameInfo.Name or "Unknown Game"
-
 --// Notify
 NotificationLibrary:SendNotification("Info", Localization.analyzing, 3)
 task.wait(1)
