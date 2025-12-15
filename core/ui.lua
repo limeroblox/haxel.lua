9e93294939e798bb7f1fde80b0ecb9509159cc4c
Lime <limeroblox2019@gmail.com>
2025-12-15 13:35:55 -0500

    Update ui.lua

--- core/ui.lua
+++ core/ui.lua
@@ -9,3 +9,17 @@ local repo = "https://raw.githubusercontent.com/deividcomsono/Obsidian/main/"
 local Library = loadstring(game:HttpGet(repo .. "Library.lua"))() 
 local ThemeManager = loadstring(game:HttpGet(repo .. "addons/ThemeManager.lua"))() 
 local SaveManager = loadstring(game:HttpGet(repo .. "addons/SaveManager.lua"))()
+local Options = Library.Options
+local Toggles = Library.Toggles
+Library.ForceCheckbox = false -- Forces AddToggle to AddCheckbox
+Library.ShowToggleFrameInKeybinds = true -- Make toggle keybinds work inside the keybinds UI (aka adds a toggle to the UI). Good for mobile users (Default value = true)
+local Window = Library:CreateWindow({
+  AutoShow = true,
+  MobileButtonsSide = "Left",
+  Center = true,
+	Title = "MeowWare.lua",
+	Footer = "VER: 0.0.1 [DEV]",
+	Icon = 95816097006870, -- Uses Roblox's AssetID Icon's
+	NotifySide = "Right",
+	ShowCustomCursor = false,
+})
