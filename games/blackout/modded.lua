002f525787033eb13c8f6f8af819a9936963b2ff
Lime <limeroblox2019@gmail.com>
2026-01-07 13:02:52 -0500

    Update modded.lua

--- games/blackout/modded.lua
+++ games/blackout/modded.lua
@@ -1,2 +1,6 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/limeroblox/meowware.lua/refs/heads/main/core/ui.lua"))()
 
+local Tabs = {
+	["Main"] = Window:AddTab("Main", ""),
+	["UI Settings"] = Window:AddTab("UI Settings", "settings"),
+}
