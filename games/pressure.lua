517ac8fb4f0007973c1dde034093520f99378346
Lime <limeroblox2019@gmail.com>
2026-01-08 13:42:17 -0500

    Update pressure.lua

--- games/pressure.lua
+++ games/pressure.lua
@@ -1,1 +1,10 @@
 -- [ UNDERGOING REWORK ] --
+local repo 			= "https://raw.githubusercontent.com/deividcomsono/Obsidian/main/" 
+local Library 		= loadstring(game:HttpGet(repo .. "Library.lua"))() 
+local ThemeManager 	= loadstring(game:HttpGet(repo .. "addons/ThemeManager.lua"))() 
+local SaveManager 	= loadstring(game:HttpGet(repo .. "addons/SaveManager.lua"))()
+local Options 		= Library.Options
+local Toggles 		= Library.Toggles
+local Version 		= "MeowWare.lua | VER: 0.0.1 [DEV]"
+Library.ForceCheckbox = false 
+Library.ShowToggleFrameInKeybinds = true 
