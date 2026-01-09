15a115fd93e368c9a0c03d6747d8cda83c158d92
Lime <limeroblox2019@gmail.com>
2026-01-09 12:47:13 -0500

    Update modded.lua

--- games/blackout/modded.lua
+++ games/blackout/modded.lua
@@ -1,4 +1,4 @@
-local repo 			= "https://raw.githubusercontent.com/deividcomsono/Obsidian/main/" 
+local repo 			= "https://raw.githubusercontent.com/limeroblox/meowware.lua/core/uilib_stuff"
 local Library 		= loadstring(game:HttpGet(repo .. "Library.lua"))() 
 local ThemeManager 	= loadstring(game:HttpGet(repo .. "addons/ThemeManager.lua"))() 
 local SaveManager 	= loadstring(game:HttpGet(repo .. "addons/SaveManager.lua"))()
