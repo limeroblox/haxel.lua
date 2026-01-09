f6887781547c26a46bab73e90d92b55ffc031ff8
Lime <limeroblox2019@gmail.com>
2026-01-09 14:03:41 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -36,7 +36,11 @@ if BlacklistedGames[GameId] or BlacklistedGames[PlaceId] then
     return
 end
 
-NotificationLibrary:SendNotification("Info", string.format(Localization.Analyzing), gameName, 3)
+NotificationLibrary:SendNotification(
+    "Info", 
+    string.format(Localization.Analyzing),
+    3
+)
 
 
 local scriptName = SupportedGames[PlaceId]
